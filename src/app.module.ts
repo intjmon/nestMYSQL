fed8a221b2ef45ec910ab3c08aa9849e94f56fc1
intjmon <119802324+intjmon@users.noreply.github.com>
2023-01-08 23:28:29 +0900

    mysql 접속 정보 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // 이렇게 해서 process.env에 접근할 수 있게 해준다.
+      isGlobal: true, // 이 옵션은 모든 모듈에서 process.env에 접근할 수 있게한다
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: process.env.MYSQL_HOST,
       port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       entities: [],
       synchronize: true,
     }),
